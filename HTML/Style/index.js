581ad889854c24d3a2cf6c6e16474147a155b99b
Stianjp <114061998+Stianjp@users.noreply.github.com>
2023-09-04 10:00:30 +0200

    Kommenterer i kode, endrer farge på border intro

--- HTML/Style/index.js
+++ HTML/Style/index.js
@@ -1,5 +1,5 @@
 /* Javascript for tilbakemeldingsboksen */
 function sendMelding(){
-            var meldingsboks = document.getElementById("melding");
-            meldingsboks.value = "Tilbakemelding sendt!";
+            var meldingsboks = document.getElementById("melding"); 
+            meldingsboks.value = "Tilbakemelding sendt!"; /*Når knapp er trykket kommer tekst ut*/
         }
