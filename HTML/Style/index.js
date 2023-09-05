7001f1522cc5384ca628fe565c93577760e99fe4
Stianjp <114061998+Stianjp@users.noreply.github.com>
2023-09-05 12:02:54 +0200

    Endret litt på en rekke ting. Enda feil på reaktiv

--- HTML/Style/index.js
+++ HTML/Style/index.js
@@ -3,3 +3,20 @@ function sendMelding(){
             var meldingsboks = document.getElementById("melding"); 
             meldingsboks.value = "Tilbakemelding sendt!"; /*Når knapp er trykket kommer tekst ut*/
         }
+
+
+/*Javascript for navbar for mindre skjerm*/
+/*Toggle-funksjon for mobilmeny*/
+function toggleMobilMeny () {
+    var mobilmeny = document.getElementById("mobilmeny");
+    if (mobilmeny.style.display === "none" || mobilmeny.style.display === "") {
+        mobilmeny.style.display = "flex";
+    } else{
+        mobilmeny.style.display = "none";
+    }
+}
+
+/*Hendelse for å åpne mobilmeny*/
+document.getElementById("menyToggle").addEventListener("click", toggleMobilMeny);
+/*hendelse for å lukke mobilmeny*/
+document.getElementById("lukkMeny").addEventListener("click", toggleMobilMeny);
