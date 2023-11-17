33fcaff28e404bb3a456858eca01247b249715d3
dennisbrill <dennisbrill@hotmail.com>
2023-11-17 15:59:09 +0100

    kommentert masse kode

--- Javascript/data.js
+++ Javascript/data.js
@@ -1,5 +1,13 @@
+/*
+her har jeg lagt inn litt javascript for å få mer "liv" i slideshowet som ligger på datatorg siden.
+*/ 
 
-const observer = new IntersectionObserver((entries) => {
+
+
+/*når slideshowet som er classet .hiddendata kommer opp for seeren av siden legger den til klasen 'show' 
+som gjør at den blir synlig på siden (se css koden). og når brukeren fjerner siden forsvinner slideshowet igjen. ("else" delen)
+*/
+const observer = new IntersectionObserver((entries) => {   
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting){
